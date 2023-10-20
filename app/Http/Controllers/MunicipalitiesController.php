51997932ebcbd0a4013437dd26d4d42f22437ab4
unknown <pivow@hotmail.com>
2023-10-20 01:34:20 -0300

    Added code comments for improved code readability and understanding. Implemented pagination for listing municipalities to enhance navigation of extensive results.

--- app/Http/Controllers/MunicipalitiesController.php
+++ app/Http/Controllers/MunicipalitiesController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,8 +9,15 @@ use GuzzleHttp\Exception\RequestException;
 
 class MunicipalitiesController extends Controller
 {
+    /**
+     * Retorna uma lista paginada de municípios de uma UF.
+     *
+     * @param string $uf Código da Unidade Federativa (ex: RS).
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index($uf)
     {
+        // Obtém o provedor de API a ser usado (brasilapi ou ibge)
         $apiProvider = strtolower(env('API_PROVIDER', 'brasilapi'));
 
         if ($apiProvider === 'brasilapi') {
@@ -41,11 +49,26 @@ class MunicipalitiesController extends Controller
             }
         }
 
-        return response()->json(['data_provider' => $dataProvider, 'municipalities' => $municipalities]);
+        // Implementação da paginação dos resultados
+        $page = request('page', 1);
+        $perPage = 10;
+        $municipalitiesPaginated = array_slice($municipalities, ($page - 1) * $perPage, $perPage);
+
+        return response()->json([
+            'data_provider' => $dataProvider,
+            'municipalities' => $municipalitiesPaginated,
+            'current_page' => $page,
+            'total' => count($municipalities),
+            'per_page' => $perPage
+        ]);
     }
 
-
-
+    /**
+     * Formata os dados da API para o formato desejado.
+     *
+     * @param array $apiData Dados da API.
+     * @return array
+     */
     private function formatApiData($apiData)
     {
         $formattedData = [];
